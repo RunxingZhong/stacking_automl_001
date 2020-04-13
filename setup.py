f89bc0d361f93a3fa16766a1704c03a564d83a00
zhongrunxing <zhongrunxing@4paradigm.com>
2020-04-13 14:42:49 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "stacking_automl",
+    version = "0.0.1",
+    license = "MIT Licence",
+
+    url = "https://github.com/RunxingZhong/stacking_automl_001",
+    author = "zhongrunxing",
+    author_email = "zhongrunxing@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
