47f8fe5463973fa1945e1f2151a02990a754a62f
zhongrunxing <zhongrunxing@4paradigm.com>
2020-04-13 19:10:56 +0800

    new

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "stacking_automl",
-    version = "0.0.1",
+    version = "0.0.2",
     license = "MIT Licence",
 
     url = "https://github.com/RunxingZhong/stacking_automl_001",
--- stacking_automl.py
+++ stacking_automl.py
@@ -1,5 +1,21 @@
-def stacking_automl(name = 'tianchi'):
-    if name == 'tianchi':
-        print('stacking_automl...')
-    else:
-        print('other') 
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import numpy as np
+import pandas as pd
+import runningz_automl
+
+def stacking_automl(train, test, col_id, col_label, cols_fea, cols_cat, meta_train_list, meta_test_list):
+    try:
+        sub = runningz_automl.automl_adapter(train, test, col_id, col_label, cols_fea, cols_cat, 
+            meta_train_list, meta_test_list, key = 'f1c1592588411002af340cbaedd6fc33')
+        print('[+] done')
+    except:
+        print('[+] error')
+        sub = test[[col_id]]
+        sub[col_label] = 0
+    return sub
+
+def show():
+    print('show test info...')
+    
